85538595dcaea15d409153a903783e4c3667f0b0
Lim Xin Yi <109676496+xinyi0227@users.noreply.github.com>
2024-07-29 22:32:16 +0800

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,4 @@
+function setCredentials() {
+  PropertiesService.getScriptProperties().setProperty('NOTION_API_KEY', 'secret_sOpk3HQUqVCS2frhJT5aQFGX8CMBS5fQBayYYfpJkLI');
+  PropertiesService.getScriptProperties().setProperty('DATABASE_ID', '2d4adcdbb67c4aaa8fcf3b184ff58f99');
+}
